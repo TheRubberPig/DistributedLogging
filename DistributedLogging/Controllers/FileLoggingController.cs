06dc6047f1e2142ca3c3a4035ecf8a96ac06b3ef
Dave <Dave@DESKTOP-DRP6QFL>
2020-10-23 09:25:21 +0100

    Added wireframe of log levels to file logging controller

--- DistributedLogging/Controllers/FileLoggingController.cs
+++ DistributedLogging/Controllers/FileLoggingController.cs
@@ -26,5 +26,33 @@ namespace DistributedLogging.Controllers
             _logger.LogError("Test Error Log");
             return Ok("Pong");
         }
+
+        [HttpPost("Log Info Message")]
+        public ActionResult<string> LogInfoMessage(string message)
+        {
+            _logger.LogInformation(message);
+            return Ok(string.Format("Logged message: {0} to file", message));
+        }
+
+        [HttpPost("Log Debug Message")]
+        public ActionResult<string> LogDebugMessage(string message)
+        {
+            _logger.LogDebug(message);
+            return Ok(string.Format("Logged message: {0} to file", message));
+        }
+
+        [HttpPost("Log Warning Message")]
+        public ActionResult<string> LogWarningMessage(string message)
+        {
+            _logger.LogWarning(message);
+            return Ok(string.Format("Logged message: {0} to file", message));
+        }
+
+        [HttpPost("Log Error Message")]
+        public ActionResult<string> LogErrorMessage(string message)
+        {
+            _logger.LogError(message);
+            return Ok(string.Format("Logged message: {0} to file", message));
+        }
     }
 }
